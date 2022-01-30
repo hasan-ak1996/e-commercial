71bdb2eb2d05aca679bc6ad5ee6b43c77ac6fdf2
hasan <hasan@DESKTOP-907M8LS>
2022-01-30 23:20:35 +0200

    Add project files.

---
+++ e-commercial-API/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using e_commercial_Domain;
+using e_commercial_Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace e_commercial_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public StoreContext _context { get; }
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> Products()
+        {
+            var prducts = await _context.Products.ToListAsync();
+            return Ok(prducts);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+
+
+    }
+}
---
+++ e-commercial-Domain/StoreContext.cs
@@ -0,0 +1,19 @@
+﻿using e_commercial_Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e_commercial_Domain
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
